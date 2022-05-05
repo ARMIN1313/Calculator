d807778f3224c46d020d5cff8199e2b74ac133e1
ARMIN1313 <96407325+ARMIN1313@users.noreply.github.com>
2022-05-05 20:44:52 +0430

    add colspan

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Armin" src="src\assets\logo.jpg" />
   <HelloWorld msg="hello armin" />
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <table id="calculator" dir="ltr" style="text-align:center;margin: auto"> </table>
-    
+    <tr>
+      <!--table row-->
+      <td colspan="5">
+        
+      </td>
+      </td>
+    </tr>
   </div>
 </template>
